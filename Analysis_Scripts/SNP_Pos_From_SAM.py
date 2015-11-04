7040f7050d6b2995a83fa36945bdb53bf295e565
TomJKono <kono.yoshitaka@gmail.com>
2015-11-04 16:59:07 -0600

    Update 9K positions script, hopefully right.

--- Analysis_Scripts/SNP_Pos_From_SAM.py
+++ Analysis_Scripts/SNP_Pos_From_SAM.py
@@ -1,143 +1,191 @@
 #!/usr/bin/env python
-
-#   A script to create a VCF from the SAM file for the barley iSelect 9k
-#   SNP genotyping query sequences. This script will only work for this
-#   dataset.
-#   Takes three arguments:
-#       1) The SAM file
-#       2) The FASTA containing the query sequences
-#       3) The reference assembly
-#   Requires Biopython
+"""Script to create a VCF positions file from a SAM file. This script is
+designed to work for only the Barley 9K SNP genotyping SNPs against the Morex
+draft assembly. Takes three
+arguments:
+    1) SAM file with contextual sequence mapping data
+    2) Illumina design sequences
+    3) Reference assembly
+This script requires Biopython."""
 
 import sys
 import re
-from Bio import SeqIO
-from Bio.Seq import Seq
+try:
+    from Bio import SeqIO
+    from Bio.Seq import Seq
+except ImportError as e:
+    print 'Please install Biopython to use this script.'
+    exit(1)
 
-#   Define the IUPAC ambiguities
-iupac = {
-        'M': ['A', 'C'],
-        'R': ['A', 'G'],
-        'W': ['A', 'T'],
-        'S': ['C', 'G'],
-        'Y': ['C', 'T'],
-        'K': ['G', 'T']
-        }
+class SNP(object):
+    """A class to hold VCF information about an individual SNP. Stores the
+    following information:
+        SNP ID
+        Contig
+        Reference Position
+        Reference Base
+        Alternate Base
+        Context Sequence Length
+        Reverse Complement?
+        Context. Seq. Mapping Location
+        The query sequence itself (from SAM)
+    """
+    def __init__(self):
+        self.snpid = None
+        self.contig = None
+        self.position = None
+        self.states = None
+        self.reference = None
+        self.alternate = None
+        self.clen = None
+        self.rc = None
+        self.mappingloc = None
 
-#   Define the VCF header
-header = '''##fileformat=VCFv4.1
-##INFO=<IS=s,Number=1,Type=Flag,Description="Variant is calculated from SAM">
+    def parse_illumina(self, snp_id, illumina):
+        """Get the contextual sequence length from the Illumina design sequence.
+        Uses a regular expression to search for the '[' that encloses the query
+        polymorphism. Also extracts the query states from the Illumina probe
+        design."""
+        self.snpid = snp_id
+        #   Use re.I so that we do case-insensitive match.
+        offset = re.search('^[ATCGMRWSYKVHDBN]+\[', illumina, re.I)
+        #   .group() returns the matching sequence
+        clen = len(offset.group(0))
+        self.context_len = clen
+        #   We will split on the forward slash (/) to separate the two SNP
+        #   states. Some are lowercase for some reason, so we have to force
+        #   uppercase
+        halves = illumina.split('/')
+        self.states = [halves[0][-1].upper(), halves[1][0].upper()]
+
+    def get_mapping_data(self, sam_flag, sam_chr, sam_pos, sam_seq):
+        """Set the appropriate mapping data flags, given the information in the
+        SAM file. We are interested in the bitwise flag, the contig that the
+        context sequence maps to, and the mapping position."""
+        #   Check if the 5th (16) bit is set. If so, we need to RC
+        if int(sam_flag) & 16:
+            self.rc = True
+        else:
+            self.rc = False
+        #   It is easy to get the contig and mapping position
+        self.contig = sam_chr
+        self.mappingloc = int(sam_pos)
+        self.qseq = sam_seq
+
+    def calculate_ref_pos(self):
+        """Calculate the reference position on the Morex assembly given the
+        contextual sequence length, whether or not the sequence was RCed in
+        mapping, and the mapping position of the contextual sequence."""
+        if self.rc:
+            #   If we reverse-complement, then we have to count from the end
+            #   and add to our mapping position accordingly
+            self.position = self.mappingloc + \
+                len(self.qseq) - (self.context_len - 1)
+        else:
+            #   Otherwise it's a simple addition
+            self.position = self.mappingloc + self.context_len
+
+    def get_bases(self, refseq):
+        """Check the base in the reference sequence to try to identify the
+        correct reference and alternate state."""
+        #   Get the actual base out of the reference assembly. Remember to
+        #   subtract 1 for 0-based indexing
+        morex_base = refseq[self.contig][self.position-1]
+        self.reference = morex_base
+        #   Then check if the SNP state was RCed when it was mapped. If so,
+        #   then we need to complement the allele states before we check for
+        #   ref or not.
+        if self.rc:
+            alleles = [str(Seq(a).complement()) for a in self.states]
+        else:
+            alleles = self.states
+        #   Cast the states to a set so we can easily discard one of them
+        alleles = set(self.states)
+        alleles.discard(morex_base)
+        self.alternate = list(alleles)[0]
+
+    def print_vcf(self):
+        """Just print the data in the order that the VCF wants."""
+        toprint = [
+            self.contig,
+            str(self.position),
+            self.snpid,
+            self.reference,
+            self.alternate,
+            '.',
+            '.',
+            's'
+            ]
+        print '\t'.join(toprint)
+
+
+def read_reference(refseq):
+    """Read the reference sequence, and parse it into a dictionary that we can
+    pull out individual contigs."""
+    rseq = SeqIO.to_dict(refseq, 'fasta')
+    return rseq
+
+
+def main(sam, illumina, refseq):
+    """Main function."""
+    #   Read the reference into a dictionary
+    reference = SeqIO.to_dict(SeqIO.parse(refseq, 'fasta'))
+    #   Define the VCF header
+    header = '''##fileformat=VCFv4.2
+##INFO<ID=s,Number=1,Type=Flag,Description="Variant is calculated from SAM">
 #CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO'''
+    print header
+    #   Start an empty dictionary that will carry our SNP data throughout the
+    #   SAM and design sequence parsing
+    snp_data = {}
+    with open(illumina, 'r') as f:
+        for line in f:
+            tmp = line.strip().split()
+            #   Start a new SNP object
+            s = SNP()
+            s.parse_illumina(tmp[0], tmp[2])
+            snp_data[tmp[0]] = s
 
-#   A function to get the reference and alternate states given the SNP data
-def get_states(amb, contig, position):
-    if position >= len(ref[contig]):
-        return ('N', 'N')
-    else:
-        reference_state = ref[contig][position]
-        #   This is ugly, but it works...
-        alt_state = [i for i in iupac[amb] if i != reference_state][0]
-        return (reference_state, alt_state)
+    #   Then go through the SAM file
+    with open(sam, 'r') as f:
+        for line in f:
+            #   Skip headers
+            if line.startswith('@'):
+                continue
+            else:
+                tmp = line.strip().split()
+                #   Unpack the important information
+                snpname = tmp[0]
+                flag = int(tmp[1])
+                ctg = tmp[2]
+                map_pos = tmp[3]
+                cigar = tmp[5]
+                seq = tmp[9]
+                #   Check if the contig == *
+                #   if it is, then the SNP is unmapped.
+                if ctg == '*':
+                    continue
+                #   Then check the CIGAR string. If it is not only match, then
+                #   skip it, too. This is an area that can be improved.
+                if not re.match('^[1-9]+M$', cigar):
+                    continue
+                #   For whatever reason, if the SNP in the SAM is not in the
+                #   Illumina design sequence dictionary...
+                if snpname not in snp_data:
+                    continue
+                #   Set the SAM mapping information
+                snp_data[snpname].get_mapping_data(
+                     flag,
+                     ctg,
+                     map_pos,
+                     seq
+                     )
+                #   Then calculate the position in the reference
+                snp_data[snpname].calculate_ref_pos()
+                #   And get the correct bases
+                snp_data[snpname].get_bases(reference)
+                #   And print out the VCF line
+                snp_data[snpname].print_vcf()
 
-#   A function to get the actual offset of the SNP, rather than rely 
-#   on the expected
-def get_context(sequence, expected_context):
-    if len(sequence) > expected_context:
-        if sequence[expected_context] in iupac and expected_context < len(sequence):
-            return (sequence[expected_context], expected_context)
-        elif sequence[-expected_context] in iupac and expected_context < len(sequence):
-            return (sequence[-expected_context], len(sequence) - expected_context)
-        else:
-            #   A last resort!
-            for i, b in enumerate(sequence):
-                if b in iupac:
-                    return (b, i)
-    else:
-        for i, b in enumerate(sequence):
-            if b in iupac:
-                return (b, i)
-
-#   Read in the FASTA file for the SNPs.
-snps = SeqIO.to_dict(SeqIO.parse(sys.argv[2], 'fasta'))
-
-#   Read in the reference sequence, and ocnvert it to a dictionary
-#   !!! This takes a lot of time and memory !!!
-ref = SeqIO.to_dict(SeqIO.parse(sys.argv[3], 'fasta'))
-
-alignment_data = {}
-#   Start reading in the file
-with open(sys.argv[1], 'r') as f:
-    for line in f:
-        #   Skip the sequence header lines
-        if line.startswith('@'):
-            continue
-        else:
-            tmp = line.strip().split('\t')
-            snp_name = tmp[0]
-            #   Get the contextual sequence length
-            if snp_name.startswith('11_'):
-                clen = 120
-            elif snp_name.startswith('12_') or snp_name.startswith('3_'):
-                clen = 60
-            elif snp_name.startswith('SCRI'):
-                clen = 60
-            flag = int(tmp[1])
-            contig = tmp[2]
-            start = int(tmp[3])
-            cigar = tmp[5]
-            fastaseq = snps[snp_name]
-            samseq = tmp[9]
-            amb, offset = get_context(fastaseq, clen)
-            if snp_name in alignment_data:
-                snp_name = snp_name + '_2'
-            alignment_data[snp_name] = {'flag':flag, 
-                                        'contig':contig, 
-                                        'start':start, 
-                                        'cigar':cigar, 
-                                        'fastaseq':fastaseq, 
-                                        'samseq':samseq,
-                                        'amb':amb, 
-                                        'offset':offset, 
-                                        'name':snp_name}
-
-print header
-#   Start iterating through the alignment data, and getting the reference
-#   and alternate states
-for snp_name, snp_data in alignment_data.iteritems():
-    #   If the contig is *, then the contextual sequence is unmapped
-    if snp_data['contig'] == '*':
-        continue
-    else:
-        #   Count up the soft clipped bases. 
-        #   We count on both sides, since soft clipping can happen at the front
-        #   or the back end of a read
-        #   We don't have to worry as much about hard clipping, since the bases
-        #   in SEQ will tell us
-        frontclip = re.search('^[1-9]+S', snp_data['cigar'])
-        endclip = re.search('[1-9]+S$', snp_data['cigar'])
-        #   if there is no clipping, then we get a NoneType object
-        if frontclip:
-            #   .group() returns the matching part of the string
-            #   we slice off the last character since that is S, and will not
-            #   cast to integer nicely
-            frontclip_offset = int(frontclip.group(0)[:-1])
-        else:
-            #   If it's NoneType, then we set the offset to 0
-            frontclip_offset = 0
-        #   And the same for clipping on the end of the read
-        if endclip:
-            endclip_offset = int(endclip.group(0)[:-1])
-        else:
-            endclip_offset = 0
-        #   Check the flag, if it's 16, then we have to flip the positions
-        #   And use the clipping on the end of the read instead of the front
-        if snp_data['flag'] == 16 or (snp_data['flag'] - 256) == 16:
-            pos = snp_data['start'] + (len(snp_data['fastaseq']) - endclip_offset) - snp_data['offset']
-            a = str(Seq(snp_data['amb']).complement())
-        else:
-            pos = snp_data['start'] + snp_data['offset'] - frontclip_offset
-            a = snp_data['amb']
-        refbase ,altbase = get_states(a, snp_data['contig'], pos)
-        #   A quick sanity check
-        if refbase in iupac[a] and altbase in iupac[a]:
-            print '\t'.join([snp_data['contig'], str(pos+1), snp_data['name'], refbase, altbase, '.', '.', 's'])
+#   Run the main function
+main(sys.argv[1], sys.argv[2], sys.argv[3])
